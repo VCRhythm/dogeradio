1a9c8f855a9abbf6a7ecbef44c84a381c9c84f08
Chris Hardy <chrishardy@Chris-Hardys-iMac.local>
2014-01-14 22:20:56 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogeradio::Application.config.secret_key_base = '8c461840a101ac7039516256f4b44081821da07c12a77afe951c66d0cfee006a918472580107d314f54ee5bfbd0cfc11fc2bfa2e429a41bbb3e1e77214467fa8'
