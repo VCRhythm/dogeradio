3aa172a460f44de9b7723ad22ca0fc83df27a92e
¨Tristan <tthard@gmail.com>
2014-01-22 17:23:58 -0500

    Fixed streaming

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 Paperclip::Attachment.default_options.merge!(
-	url: ':s3_domain_url',
-	path: ':class/:attachment/:id/:style/:filename',
+	url: ':s3_path_url',
+	path: ':filepath',
 	storage: :s3,
 	s3_credentials: Rails.configuration.aws,
 	s3_permissions: :private,
