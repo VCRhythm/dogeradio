bd7912689784fe5681982bc04a5333c8cf11ac50
¨Tristan <tthard@gmail.com>
2014-01-22 23:52:47 -0500

    Music dependent on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :musics
+  has_many :musics, dependent: :destroy
 
 end
