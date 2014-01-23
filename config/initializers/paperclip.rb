8c277c7be92889cdd3dc9c24643a7d7ca7bd827e
¨Tristan <tthard@gmail.com>
2014-01-23 10:02:08 -0500

    Messed up upload"

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,1 @@
-Paperclip::interpolates :direct_upload_url do |attachment, style|
-	attachment.instance.direct_upload_url
-end
 
-Paperclip::Attachment.default_options.merge!(
-	url: ':s3_path_url',
-	path: ':attachment/:id/:filename',
-	storage: :s3,
-	s3_credentials: Rails.configuration.aws,
-	s3_permissions: :private,
-	s3_protocol: 'https'
-)
