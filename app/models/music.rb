31035b1ee627dcb08867cbf0a073f0639c9783be
¨Tristan <tthard@gmail.com>
2014-01-25 00:00:04 -0500

    Hopefully fix files

--- app/models/music.rb
+++ app/models/music.rb
@@ -46,13 +46,13 @@ class Music < ActiveRecord::Base
   def self.transfer_and_cleanup(id)
 	  music = Music.find(id)
 		direct_upload_url_data = DIRECT_UPLOAD_URL_FORMAT.match(music.direct_upload_url)
-		clean_direct_upload_url_data = direct_upload_url_data[:filename].gsub!(/'/, '')
+		clean_direct_upload_url = direct_upload_url_data[:filename].gsub!(/'/, '')
 	  s3 = AWS::S3.new
 																					    
 	  if music.post_process_required?
 		  music.upload = URI.parse(URI.escape(music.direct_upload_url))
 	  else
-	    paperclip_file_path = "musics/uploads/#{id}/original/#{clean_direct_upload_url_data[:filename]}"
+	    paperclip_file_path = "musics/uploads/#{id}/original/#{clean_direct_upload_url}"
 	    s3.buckets[Rails.configuration.aws[:bucket]].objects[paperclip_file_path].copy_from(direct_upload_url_data[:path])
 	  end
 		
