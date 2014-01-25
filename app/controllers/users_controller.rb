31b541ad4b78895f28fc8b7ba4f40c1c72a9093f
¨Tristan <tthard@gmail.com>
2014-01-25 00:49:10 -0500

    Save deduction from account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 	def pay
+		@user = current_user
     @user_to_pay = User.find(params[:user_id])
 		@music = Music.find(params[:music_id])
 		@amount = 5
 
-		if current_user.balance > @amount
-			current_user.balance -= @amount
+		if @user.balance > @amount
+			@user.balance -= @amount
 			@user_to_pay.balance += @amount
+			@user.save
 			@user_to_pay.save
 		else
 			render layout: false
