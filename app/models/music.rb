202f64dc4a91449d7eb9baa46c4c21517618e954
¨Tristan <tthard@gmail.com>
2014-01-25 01:28:40 -0500

    Fix?

--- app/models/music.rb
+++ app/models/music.rb
@@ -56,8 +56,8 @@ class Music < ActiveRecord::Base
 	    s3.buckets[Rails.configuration.aws[:bucket]].objects[paperclip_file_path].copy_from(direct_upload_url_data[:path])
 	  end
 		
-		music.upload_file_name.gsub!(/'/,'')
 		music.direct_upload_url = clean_direct_upload_url
+		music.upload_file_name = music.upload_file_name.gsub(/'/,'')
 	  music.processed = true
     music.save
 	  
