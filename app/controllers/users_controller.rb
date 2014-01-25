095c88421995acbd101decd3a7fe5f089bcdd786
¨Tristan <tthard@gmail.com>
2014-01-25 00:46:17 -0500

    Fixed tipping

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,13 @@ class UsersController < ApplicationController
     @user_to_pay = User.find(params[:user_id])
 		@music = Music.find(params[:music_id])
 		@amount = 5
+
 		if current_user.balance > @amount
 			current_user.balance -= @amount
 			@user_to_pay.balance += @amount
+			@user_to_pay.save
+		else
+			render layout: false
 		end
-		@user_to_pay.save
 	end
 end
