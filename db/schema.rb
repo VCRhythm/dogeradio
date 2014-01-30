90b7f3c3eb4be933ca7284ebd8ab314fb02ebdac
¨Tristan <tthard@gmail.com>
2014-01-30 09:01:40 -0500

    Added bio to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129154020) do
+ActiveRecord::Schema.define(version: 20140130133249) do
 
   create_table "beta_codes", force: true do |t|
     t.integer  "value"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20140129154020) do
     t.float    "balance",                default: 0.0
     t.integer  "code"
     t.float    "prev_received",          default: 0.0
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
