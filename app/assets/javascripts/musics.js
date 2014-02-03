5dffa1276c81f9882465100cbd726abb08f19493
¨Tristan <tthard@gmail.com>
2014-02-03 18:03:57 -0500

    Java

--- app/assets/javascripts/musics.js
+++ app/assets/javascripts/musics.js
@@ -42,14 +42,6 @@ function updatePlayer(music_id){
 
 $(document).ready(function(){
 	
-	$('#s3_uploader').S3Uploader({
-		remove_completed_progress_bar: false,
-		progress_bar_target: $('#uploads_container')
-	});
-
-	$('#s3_uploader').bind('s3_upload_failed', function(e, content) {
-		return alert(content.filename + " failed to upload.");
-	});
 
 	$('.remote-link').click(function(){
 		$('body').animate({scrollTop: $("#main").offset().top-90}, 500);
