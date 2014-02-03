491e92fa67649c78d6bd6e252ce73ea8a3f57068
¨Tristan <tthard@gmail.com>
2014-02-02 19:25:29 -0500

    Scroll fix

--- app/assets/javascripts/musics.js
+++ app/assets/javascripts/musics.js
@@ -57,9 +57,7 @@ function updatePlayer(music_id){
 
 $(document).ready(function(){
 	$('.remote-link').click(function(){
-		$('html, body').animate({
-			scrollTop: $("#main").offset().top-90
-		}, 500);
+		$('body').animate({scrollTop: $("#main").offset().top-90}, 500);
 	});
 
 	$('.sortable').sortable({
