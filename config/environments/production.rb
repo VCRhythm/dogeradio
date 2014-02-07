a12fb76d3b16eef94a5c495b8cd11ae5e0b4c993
¨Tristan <tthard@gmail.com>
2014-02-07 13:45:14 -0500

    Test to fix glyphicons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Dogeradio::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+	config.assets.precompile += %w( .svg .eot .woff .ttf)
 end
