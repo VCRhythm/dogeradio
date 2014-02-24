67125388ca3a068a403d7a906abb950b488dd590
¨Tristan <tthard@gmail.com>
2014-02-24 12:59:45 -0500

    Search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def create_guest_user
-		u = User.create(username: "guest", email: "guest_#{Time.now.to_i}#{rand(99)}@dogeradio.com")
+		u = User.create(username: "guest"+SecureRandom.base64, email: "guest_#{Time.now.to_i}#{rand(99)}@dogeradio.com")
 		u.save!(validate: false)
 		@playlist = u.playlists.create(name: "queue", category: "queue")
 		@playlist.tracks = Track.order(created_at: :desc).limit(20)
