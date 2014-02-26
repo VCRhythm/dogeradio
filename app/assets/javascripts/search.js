cf4fd07bec114004d0765ab96cc5653b439f7bd8
¨Tristan <tthard@gmail.com>
2014-02-26 14:14:18 -0500

    Fix autocomplete

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,5 @@
 $(function() {
-	$('#search_form').typeahead({
+	$('#search_query').typeahead({
 		name: 'tracks',
 		remote: "/autocomplete?search[query]=%QUERY"
 	}).on('typeahead:selected', function(obj, datum, name){
