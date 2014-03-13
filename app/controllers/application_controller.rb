baa2dbc4a5bf28e7983116f1122b6fd0c8aa1781
¨Tristan <tthard@gmail.com>
2014-03-12 23:55:03 -0400

    Fix location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::Base
 		if params[:location].blank?
 			if Rails.env.test? || Rails.env.development?
 				@location ||= Geokit::Geocoders::MultiGeocoder.geocode("69.243.26.55")
-			elsif session[:geo_location] != ''
+			elsif !session[:geo_location].blank?
 				@location ||= session[:geo_location]
 			else
 				@location ||= Geokit::Geocoders::MultiGeocoder.geocode("Pullman, WA, USA")
